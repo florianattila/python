d4d2b0817c5fa5d7cf8907443f7b490e4dded2b2
Attila Dávid Flórián <florianad@GDO.LAN>
2021-10-06 11:30:54 +0200

    varosok es tomeg

---
+++ tomeg.py
@@ -0,0 +1,7 @@
+import random
+
+tömegek = []
+for _ in range(20):
+    tömegek.append(random.randint(1000,10000))
+#kész a listánk
+print(tömegek)
---
+++ városokfor.py
@@ -0,0 +1,3 @@
+varosok = ["Miskolc","Párizs","Dublin","Lajosmizse"]
+for varos in varosok: 
+    print(varos, " egy város Európában")
